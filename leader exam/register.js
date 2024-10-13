96753dfda04ac900e97066610f613082286e4280
zuka <157312286+zukadzm@users.noreply.github.com>
2024-10-13 19:27:16 +0400

    Add files via upload

---
+++ leader exam/register.js
@@ -0,0 +1,10 @@
+const button2 = document.getElementById("btn1");
+const button3 = document.getElementById("btn2");
+const form = document.createElement("container2");
+
+
+button2.addEventListener("click",function() {
+    if(form === '') {
+        alert("please fill")
+    }
+})
